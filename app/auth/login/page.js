2e7d6e1a356cd19236fac7db15218e30f1a10114
Harriet <hiluekhabho@gmail.com>
2024-06-13 14:49:05 +0100

    added components/AuthButtons.js

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import React from 'react'
+import AuthButtons from '@/components/AuthButtons'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label" 
 
 const Login = () => {
   return (
