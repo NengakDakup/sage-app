7ed94e24a6a137b5665901420e0a6bfb76aa51a9
Harriet <hiluekhabho@gmail.com>
2024-06-13 15:02:04 +0100

    added public/images/google-logo.svg

---
+++ components/AuthButtons.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+const AuthButtons = () => {
+  return (
+    <div className='flex flex-col justify-center items-center gap-8 w-full'>
+      <Link href='#' className='flex w-full'>
+        <Button variant="dark" className='gap-2 grow'>
+              <Image src="/images/google-logo.svg" alt="Google logo" width={24} height={24}/>
+              <p>Continue with Google</p>
+          </Button>
+      </Link>
+      <Link href='#' className='flex w-full'>
+        <Button variant="dark" className='gap-2 grow'>
+          <Image src="/images/apple-logo.svg" alt="Apple logo" width={24} height={24} />
+          <p>Continue with Apple</p>
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+export default AuthButtons
