aa5ead46e0135ac9a095088d77e53e96d9235b27
Harriet <hiluekhabho@gmail.com>
2024-06-14 16:08:42 +0100

    used imported exit exit() Link and Images in the return block

--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -6,3 +6,19 @@ import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { X } from 'lucide-react';
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center h-full p-4 md:p-12 ">
+    <div className="relative flex flex-col w-full h-min max-w-[743px] rounded-xl gap-12 py-12 px-4 md:px-16 my-auto md:gradient-border-darkest">
+    <Link href='#' className=' hidden md:block'>
+      <Image src='/images/x-mark-close.svg' alt='X-mark' width={24} height={24} className='absolute right-[0px] hidden md:block'/> 
+    </Link>
+        <div className='flex flex-col md:flex-row gap-6 justify-center items-center'>
+            <Image src='/images/logo.png' alt='SageAI' width={36} height={36} />
+            <h1 className='font-bold text-xl md:text-3xl'>Get Started</h1>
+        </div>
+  )
+}
+
+export default page
