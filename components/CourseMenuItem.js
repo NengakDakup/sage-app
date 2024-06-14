74257759ec5dc4f0c16156e50a45ade5656b01c1
Nengak Dakup <nengakdakup@gmail.com>
2024-06-14 18:06:36 +0100

    Added course menu item

---
+++ components/CourseMenuItem.js
@@ -0,0 +1,21 @@
+import { BookMarked, EllipsisVertical } from 'lucide-react'
+import React from 'react'
+
+const CourseMenuItem = () => {
+  return (
+    <div className='flex flex-row items-center justify-between w-full h-16 px-4 border border-purple-light rounded-md cursor-pointer hover:bg-purple-light'>
+        <div className='flex flex-row items-center justify-start gap-2'>
+            <BookMarked className='w-6 h-6' />
+            <div className='flex flex-col'>
+                <p className='font-semibold'>Intro To AI</p>
+                <p className='text-slate-300 text-[10px]'>2 documents submitted</p>
+            </div>
+
+        </div>
+        <EllipsisVertical className='w-5 h-5' />
+
+    </div>
+  )
+}
+
+export default CourseMenuItem
