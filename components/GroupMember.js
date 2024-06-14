0d7eda2549c4392a06342bcf0445a0dbb26e5639
Nengak Dakup <nengakdakup@gmail.com>
2024-06-14 18:09:54 +0100

    Added GroupMember Icon

---
+++ components/GroupMember.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GroupMember = () => {
+  return (
+    <div className=' flex justify-center items-center w-[44px] h-[44px] bg-[#252031] border-2 border-solid border-[#695989] shadow-[-5px_3px_0px_#695989] rounded-full'>JD</div>
+  )
+}
