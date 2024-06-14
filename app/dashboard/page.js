a2d033c2f1bf9f87759c7e708623b005ea4588d5
Nengak Dakup <nengakdakup@gmail.com>
2024-06-14 18:23:43 +0100

    Added Dashboard Flashcard Lists

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,12 @@
 import DashboardChatHistory from '@/components/DashboardChatHistory'
+import DashboardFlascardsList from '@/components/DashboardFlashcardsList'
 import React from 'react'
 
 export default function DashboardPage () {
   return (
     <>
       <DashboardChatHistory />
+      <DashboardFlascardsList />
     </>
   )
 }
