1d777bb4fce5fd6afe075395a4040ba8085a103a
Nengak Dakup <nengakdakup@gmail.com>
2024-06-14 18:35:16 +0100

    Added User Chat box

---
+++ components/Userchat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Label } from "@/components/ui/label"
+export const Userchat = () => {
+  return (
+    <div className='self-end'>
+        <Label className="w-full text-gray-400 text-[8px] text-right" >GARY JAKES</Label>
+        <div className='border border-solid border-gray-600 p-3 rounded-lg text-[12px]'>
+          Hey Sage, Summarize the Introduction to AI Systems course.
+        </div>
+    </div>
+  )
+}
