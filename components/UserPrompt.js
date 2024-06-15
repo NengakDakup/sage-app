4b717f0f67d70fa19a050fa3ae7b08364386c28f
Nengak Dakup <nengakdakup@gmail.com>
2024-06-15 20:16:34 +0100

    Hide scroll bar for user prompts section

--- components/UserPrompt.js
+++ components/UserPrompt.js
@@ -3,7 +3,7 @@ import { Badge } from '@/components/ui/badge'
 
 export const UserPrompt = () => {
   return (
-    <div className='flex gap-2 pl-12 w-full overflow-x-scroll'>
+    <div className='flex gap-2 pl-12 w-full overflow-x-scroll hide-scrollbar'>
         <Badge className="text-nowrap" variant="prompt">Summarize This Course</Badge>
         <Badge className="text-nowrap" variant="prompt">Start A Quiz</Badge>
         <Badge className="text-nowrap" variant="prompt">Set a Study Reminder</Badge>
