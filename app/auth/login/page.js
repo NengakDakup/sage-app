246ea1f1ff74570a7e42725ef2f018811a9929e4
Harriet <hiluekhabho@gmail.com>
2024-06-15 17:58:25 +0100

    media quries for AddProgressBar.js

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { X } from 'lucide-react';
-import AddFlashcard from '@/components/AddFlashcard'
+import AddCourseProgressBar from '@/components/AddCourseProgressBar'
 
 const Login = () => {
   return (
@@ -49,7 +49,7 @@ const Login = () => {
             </Link>
           </div>    
         </div>
-        <AddFlashcard/>
+        <AddCourseProgressBar/>
       </div>
   )
 }
