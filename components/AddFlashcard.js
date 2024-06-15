33f5a025bf9b60095260fcba37644b7dd3289926
Harriet <hiluekhabho@gmail.com>
2024-06-15 13:31:36 +0100

    Almost done with AddFlashcards.js

--- components/AddFlashcard.js
+++ components/AddFlashcard.js
@@ -9,32 +9,19 @@ import {
   } from "@/components/ui/dialog"
   import { Input } from "@/components/ui/input" 
   import { Button } from '@/components/ui/button' 
-  import { useState } from 'react';
 
 const AddFlashcard = () => {
-    const [text, setText] = useState('');
-
-    const handleChange = (e) => {
-      setText(e.target.value);
-    };
   return (
     <div>
 <Dialog>
   <DialogTrigger>Open</DialogTrigger>
   <DialogContent className='gradient-border w-[570px]'>
     <DialogHeader>
-      <DialogTitle>Add A Flashcard</DialogTitle>
+      <DialogTitle className='pb-5'>Add A Flashcard</DialogTitle>
       <DialogDescription className='flex flex-col justify-center items-center gap-5'>
 
         <Input className='w-full' id="password1" type="text" placeholder="Give it a Name" />     
-        <textarea
-        id="myTextarea"
-        className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        value={text}
-        onChange={handleChange}
-        rows="10"
-        cols="30"
-      />
+        <Input className='w-full h-[129px] pb-24 px-3'id="password1" type="text" placeholder="Note..." />
         <Button variant="light" className="w-full">Add Flashcard</Button>
 
       </DialogDescription>
