c9005ea63e44fe7347345f066ca2343b47107b71
Harriet <hiluekhabho@gmail.com>
2024-06-16 16:48:11 +0100

    Added chevron left icon

--- components/MobileLeftSideBar.js
+++ components/MobileLeftSideBar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
+import { ChevronLeft } from 'lucide-react';
 
 const MobileLeftSideBar = () => {
   return (
-    <div className='bg-blue-darkest w-[330px] h-[852px]'>
-
+    <div className='bg-blue-darkest pt-16 w-[330px] h-[852px]'>
+        <div>
+            <Image src='/images/logo.png' alt='SageAI' width={36} height={36} />
+            <ChevronLeft />
+        </div>
+        
+        <div>
+            <Image src='/images/placeholder-image.png' alt='SageAI' width={36} height={36} /> 
+        </div>
     </div>
   )
 }
