93292c97cfb0bdd56e4b92369b1906dec5d06433
Harriet <hiluekhabho@gmail.com>
2024-06-16 18:02:04 +0100

    still updating

--- components/MobileCourseItemsMenu.js
+++ components/MobileCourseItemsMenu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, SquarePlus } from 'lucide-react';
 import { Button } from './ui/button'
 const MobileCourseItemsMenu = ({toggled, setToggled}) => {
   return (
@@ -10,11 +10,21 @@ const MobileCourseItemsMenu = ({toggled, setToggled}) => {
         </div>
 
         <div className='flex flex-col w-full mt-12 gap-4 px-4'>
-            <Button className={`w-full py-6 gap-4 ${toggled? "justify-center" : "justify-start"}`} variant="dark-dashed">
+            <Button className={`w-full py-6 gap-4 ${toggled? "justify-start" : "justify-center"}`} variant="dark-dashed">
                 <SquarePlus />
                 {!toggled && "My Course"}
             </Button>
         </div>
+
+        {!toggled &&
+            <div className='flex flex-col w-full h-full overflow-y-scroll mt-6 py-6 px-3 gap-4 border-t border-purple-light'>
+                <CourseMenuItem />
+                <CourseMenuItem />
+                <CourseMenuItem />
+
+
+            </div>
+        }
     </div>
   )
 }
