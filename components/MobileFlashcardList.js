1806de4c240857c6fd98861546f9e5dbab816194
Nengak Dakup <nengakdakup@gmail.com>
2024-06-16 23:53:22 +0100

    Modified flascard list

--- components/MobileFlashcardList.js
+++ components/MobileFlashcardList.js
@@ -2,21 +2,25 @@ import React from 'react'
 import { X, Album } from 'lucide-react';
 import { DashboardFlashcards } from './DashboardFlashcards';
 
-const MobileFlashcardList = () => {
+const MobileFlashcardList = ({setMobileRightToggled}) => {
   return (
-    <div className='bg-blue-darkest w-[330px] h-full  px-6'>
+    <div className='bg-blue-darkest w-[330px] h-full px-6 fixed right-0'>
       <div className='flex justify-between items-center py-16'>
         <div className='flex gap-3'>
             <Album />
             <p className='font-bold'>My Flashcards</p>
         </div>
-        <X className='text-[#FF7193]'/>
+        <X className='text-[#FF7193]' onClick={() => setMobileRightToggled(false)}/>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <DashboardFlashcards gradient={true}/>
+        <DashboardFlashcards />
+        <DashboardFlashcards />
+        <DashboardFlashcards />
+
       </div>
 
-      <DashboardFlashcards gradient={true}/>
-      <DashboardFlashcards />
-      <DashboardFlashcards />
-      <DashboardFlashcards />
+      
     </div>
   )
 }
