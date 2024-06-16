fbe9e85f5669eabeb2341219e0fea9f91be2f7a4
Harriet <hiluekhabho@gmail.com>
2024-06-16 17:02:40 +0100

    updating

--- components/MobileLeftSideBar.js
+++ components/MobileLeftSideBar.js
@@ -5,13 +5,20 @@ import { ChevronLeft } from 'lucide-react';
 const MobileLeftSideBar = () => {
   return (
     <div className='bg-blue-darkest pt-16 w-[330px] h-[852px]'>
-        <div>
+        <div className='w-full flex justify-between items-center'>
             <Image src='/images/logo.png' alt='SageAI' width={36} height={36} />
             <ChevronLeft />
         </div>
         
-        <div>
-            <Image src='/images/placeholder-image.png' alt='SageAI' width={36} height={36} /> 
+        <div className='flex justify-start items-end gap-1'>
+            <div>
+                <Image src='/images/placeholder-image.png' className='rounded-full w-[44px] h-[44px]' alt='SageAI' width={36} height={36} />
+            </div>
+             
+            <div>
+                <p className='text-xs'>John Doe</p>
+                <p className='text-xs text-red-600'>Log Out</p>
+            </div>
         </div>
     </div>
   )
