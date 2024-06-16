243a9da8ed4f33f96b9c9592f05c890abdb8424f
Nengak Dakup <nengakdakup@gmail.com>
2024-06-16 23:53:44 +0100

    Modified left sidebar

--- components/MobileLeftSideBar.js
+++ components/MobileLeftSideBar.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { ChevronLeft, SquarePlus } from 'lucide-react';
 import { Button } from './ui/button'
 import CourseMenuItem from './CourseMenuItem';
-const MobileCourseItemsMenu = ({toggled, setToggled}) => {
+
+const MobileCourseItemsMenu = ({setMobileLeftToggled}) => {
   return (
-    <div className='bg-blue-darkest w-[330px] h-full flex flex-col justify-center items-start'>
+    <div className='bg-blue-darkest w-[330px] h-full flex flex-col justify-center items-start fixed z-100000'>
         <div className='w-full flex justify-between items-center pt-16 px-4'>
             <p className='text-[17px] font-bold'>My Courses</p>
-            <ChevronLeft />
+            <ChevronLeft onClick={() => setMobileLeftToggled(false)} />
         </div>
 
         <div className='flex flex-col w-full mt-12 gap-4 px-4'>
-            <Button className={`w-full py-6 gap-4 ${toggled? "justify-center" : "justify-start"}`} variant="dark-dashed">
+            <Button className={`w-full py-6 gap-4 $justify-center`} variant="dark-dashed">
                 <SquarePlus />
-                {!toggled && "New Course"}
+                New Course
             </Button>
 
         </div>
