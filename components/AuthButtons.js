df4bc8afe7cf2f6874dd0a2a7298ff9f43fd2d7b
Nengak Dakup <nengakdakup@gmail.com>
2024-06-17 01:15:51 +0100

    Removed Apple sign in button

--- components/AuthButtons.js
+++ components/AuthButtons.js
@@ -6,18 +6,18 @@ import { Button } from '@/components/ui/button'
 const AuthButtons = () => {
   return (
     <div className='flex flex-col justify-center items-center gap-8 w-full'>
-      <Link href='http://localhost:4000/api/auth/google' className='flex w-full'>
+      <Link href={`${process.env.SERVER}/api/auth/google`} className='flex w-full'>
         <Button variant="dark" className='gap-2 grow'>
               <Image src="/images/google-logo.svg" alt="Google logo" width={24} height={24}/>
               <p>Continue with Google</p>
           </Button>
       </Link>
-      <Link href='#' className='flex w-full'>
+      {/* <Link href='#' className='flex w-full'>
         <Button variant="dark" className='gap-2 grow'>
           <Image src="/images/apple-logo.svg" alt="Apple logo" width={24} height={24} />
           <p>Continue with Apple</p>
         </Button>
-      </Link>
+      </Link> */}
     </div>
   )
 }
